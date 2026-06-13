c350982bfe3acec9d3a8bb0d996fe22fe2dfd2e9
TOWUK <towuk@internet.ru>
2026-06-13 21:56:19 +0300

    +

---
+++ src/AdminCmd.java
@@ -0,0 +1,31 @@
+import arc.util.CommandHandler;
+import arc.util.Reflect;
+import mindustry.Vars;
+import mindustry.core.GameState;
+import mindustry.gen.Player;
+import mindustry.maps.Map;
+import mindustry.mod.Plugin;
+public class AdminCmd extends Plugin {
+    private CommandHandler sh;
+    @Override
+    public void init() { sh = arc.Core.app.getListeners().find(l -> l.getClass().getSimpleName().equals("ServerControl")) instanceof Object c ? Reflect.get(c, "handler") : null; }
+    @Override
+    public void registerClientCommands(CommandHandler h) {
+        h.register("p", "Toggle pause", (String[] a, Player p) -> { if (!p.admin) return; Vars.state.set(Vars.state.is(GameState.State.paused) ? GameState.State.playing : GameState.State.paused); });
+        h.register("m", "<name...>", "Change map or /m l", (String[] a, Player p) -> {
+            if (!p.admin) return; if (a[0].equalsIgnoreCase("l")) {
+                var maps = Vars.maps.customMaps();
+                if (maps.isEmpty()) { return; }
+                StringBuilder sb = new StringBuilder("Maps:\n");
+                maps.each(m -> sb.append(m.plainName()).append("\n"));
+                p.sendMessage(sb.toString());
+                return;
+            }
+            String search = a[0].replace("_", " ").toLowerCase();
+            Map map = Vars.maps.all().find(m -> m.plainName().toLowerCase().contains(search));
+            if (map == null) return;
+            sh.handleMessage("nextmap " + map.plainName().replace(" ", "_"));
+            sh.handleMessage("gameover");
+        });
+    }
+}
